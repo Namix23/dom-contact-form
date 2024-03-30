2f4ef0688781d3fd887aabe92b7f24af583b0f6b
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-30 02:50:03 -0400

    create new li element

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ form.addEventListener('submit', event => {
 //use DOM manipulation to add the entered info to our list
 const nameInput = document.querySelector('#name');
 const numberInput = document.querySelector("#phone");
+const contactList = document.getElementById('contact-list'); 
+
 function addContact(){
   if (nameInput.value == "" || numberInput.value == "");
   alert('contact is missing info');
   return;
 }
+
+//add entry to contact list
+let listItem = document.createElement ('li');
