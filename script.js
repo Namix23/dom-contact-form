a92ae9dfd47da7b30ebe08c8e9713d7754b51c81
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-30 03:07:33 -0400

    add missing curly braces

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 //handle form submission
-const form = document.getElementById('contact-form');
-form.addEventListener('submit', event => {
+const form = document.getElementById("contact-form");
+form.addEventListener("submit", event => {
   event.preventDefault();
   addContact();
 });
 
 //use DOM manipulation to add the entered info to our list
-const nameInput = document.querySelector('#name');
+const nameInput = document.querySelector("#name");
 const numberInput = document.querySelector("#phone");
-const contactList = document.getElementById('contact-list'); 
+const contactList = document.getElementById("contact-list"); 
 
-function addContact(){
-  if (nameInput.value == "" || numberInput.value == "");
-  alert('contact is missing info');
+function addContact() {
+  if (nameInput.value == "" || numberInput.value == ""){
+  alert("contact is missing info");
   return;
 }
 
 //add entry to contact list
-let listItem = document.createElement ('li');
-contactList = .append(listItem);
+let listItem = document.createElement ("li");
+contactList.append(listItem);
 listItem.innerHTML = `${nameInput.value} ${numberInput.value}`;
+}
