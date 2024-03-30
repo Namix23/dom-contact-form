b92a1d00278f1bcdcc29d8ca6ff6210b2b707fbc
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-30 04:48:55 -0400

    insert missing (;) where (,) was

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 //handle form submission
 const form = document.getElementById("contact-form");
-form.addEventListener("submit", event => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   addContact();
+  formatContacts();
 });
 
 //use DOM manipulation to add the entered info to our list
 const nameInput = document.querySelector("#name");
 const numberInput = document.querySelector("#phone");
-const contactList = document.getElementById("contact-list"); 
+const contactList = document.getElementById("contact-list");
 
 function addContact() {
-  if (nameInput.value == "" || numberInput.value == ""){
-  alert("contact is missing info");
-  return;
-}
+  if (nameInput.value == "" || numberInput.value == "") {
+    alert("contact is missing info");
+    return;
+  }
 
-//add entry to contact list
-let listItem = document.createElement ("li");
-contactList.append(listItem);
-listItem.innerHTML = `<span class="name">${nameInput.value}</span> ${numberInput.value}`;
+  //add entry to contact list
+  let listItem = document.createElement("li");
+  contactList.append(listItem);
+  listItem.innerHTML = `<span class="name">${nameInput.value}</span> ${numberInput.value}`;
 
-nameInput.value = "";
-numberInput.value = "";
+  nameInput.value = "";
+  numberInput.value = "";
 }
 
 const formatBtn = document.getElementById("format");
 
 function formatContacts() {
-  let contacts = document.querySelectorAll("#contactList li");
+  let contacts = document.querySelectorAll("#contact-list li");
+  for (let i = 0; i < contacts.length; i++) {
+    let contact = contacts[i];
+    contact.className = "contact";
+    createX();
+  }
 }
 
-function createX(liNode){
-  
-}
+function createX(liNode) {}
 
 formatBtn.addEventListener("click", formatContacts);
