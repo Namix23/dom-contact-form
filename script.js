9ab732034e5d4eba1507bfd137c3447ed4955788
Namix23 <98861500+Namix23@users.noreply.github.com>
2024-03-30 02:33:23 -0400

    create 2 fixed variables using DOM selectors

--- script.js
+++ script.js
@@ -2,4 +2,11 @@
 const form = document.getElementById('contact-form');
 form.addEventListener('submit', event => {
   event.preventDefault();
+  addContact();
 });
+
+//use DOM manipulation to add the entered info to our list
+const nameInput = document.querySelector('#name');
+const numberInput = document.querySelector("#phone");;
+function addContact(){
+}
